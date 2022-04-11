35ccb2b5c94ca24dcf28081d1cc676672f13cdbb
Tuncay Demir <tuncaydemir682@gmail.com>
2022-04-11 22:38:01 +0300

    Create bodymassindex.java

---
+++ bodymassindex.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class bodymassindex{
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int weight;
+
+        double height, bmi;
+
+
+        System.out.print("Please enter your height (in meters) :");
+
+        height = scan.nextDouble();
+
+        System.out.print("Please enter your weight :");
+
+        weight = scan.nextInt();
+
+
+        bmi = weight / (height * height);
+
+        System.out.print("Your Body Mass Index :" + bmi);
+
+    }
+}
